7df470a1aedeada149fc0f8a1f1fdd607c50494d
EhDohWah <ehdohwah007@gmail.com>
2024-08-22 16:24:18 +0700

    Implemented Double on the exe, and display different forms

---
+++ project-practice-3/MVC-View/Ipd/PatientIPDForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace project_practice_3.MVC_View.Ipd
+{
+    partial class PatientIPDForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitleIPD = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitleIPD
+            // 
+            this.lblTitleIPD.AutoSize = true;
+            this.lblTitleIPD.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitleIPD.Location = new System.Drawing.Point(344, 118);
+            this.lblTitleIPD.Name = "lblTitleIPD";
+            this.lblTitleIPD.Size = new System.Drawing.Size(73, 20);
+            this.lblTitleIPD.TabIndex = 0;
+            this.lblTitleIPD.Text = "IPDForm";
+            // 
+            // PatientIPDForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTitleIPD);
+            this.Name = "PatientIPDForm";
+            this.Text = "PatientIPDForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitleIPD;
+    }
+}
--- project-practice-3/Program.cs
+++ project-practice-3/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using project_practice_3.MVC_View.Register;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using project_practice_3.MVC_View.Ipd;
+using project_practice_3.MVC_View.Opd;
 
 namespace project_practice_3
 {
@@ -14,9 +17,43 @@ namespace project_practice_3
         [STAThread]
         static void Main()
         {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Master());
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Master());
+
+            // Retrieve the launch count from settings or file
+            int launchCount = Properties.Settings.Default.LaunchCount;
+            Form formToOpen = null;
+
+            switch (launchCount)
+            {
+                case 0:
+                    formToOpen = new RegisterForm();
+                    break;
+                case 1:
+                    formToOpen = new PatientOPDForm();
+                    break;
+                case 2:
+                    formToOpen = new PatientIPDForm();
+                    break;
+                default:
+                    MessageBox.Show("All windows have been displayed.", "Information");
+                    break;
+            }
+
+            // Update the launch count for next time
+            Properties.Settings.Default.LaunchCount = (launchCount + 1) % 3;
+            Properties.Settings.Default.Save();
+
+            if (formToOpen != null)
+            {
+                formToOpen.Show(); // Show the form without blocking the code
+                Application.Run(); // Start the application loop
+            }
         }
     }
 }
